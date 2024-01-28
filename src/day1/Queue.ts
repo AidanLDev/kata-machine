70dc62025424ace1005ba3184411d79307aa6c01
Aidan Lowson <dev@aidanlowson.com>
2024-01-28 22:21:32 +0000

    Implementing stack, queue, bubule sort, binary list, two crystal

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        // Removing head;
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next; // set head to be the next node in-line
+        head.next = undefined;
+        return head.value; // return the old head
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }

56da40cb5e926e6fb95ed39ce1d1f7058967e3a1
AidanLDev <dev@aidanlowson.com>
2024-02-06 22:45:14 +0000

    Start working on mazeSolver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,44 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+function walk(
+    maze: string[],
+    wall: string,
+    cur: Point,
+    end: Point,
+    seen: boolean[][],
+): boolean {
+    /*
+    Our base cases
+  */
+    // 1. Off the map case
+    if (
+        cur.x < 0 ||
+        cur.x >= maze[0].length ||
+        cur.y < 0 ||
+        cur.y >= maze.length
+    ) {
+        return false;
+    }
 
+    // 2. On a wall
+    if (maze[cur.y][cur.x] === wall) {
+        return false;
+    }
+
+    // 3. At the end
+    if (cur.x === end.x && cur.y === end.y) {
+        return true;
+    }
+
+    // 4. Seen the point already
+    if (seen[cur.y][cur.x]) {
+        return false;
+    }
+
+    // Store seen
 }
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}

cdb569244545d032bb86f5fa588b88d9918cb30e
AidanLDev <dev@aidanlowson.com>
2024-02-18 21:06:13 +0000

    BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(cur: BinaryNode<number> | null, needle: number): boolean {
+    if (!cur) {
+        return false;
+    }
 
+    if (cur.value === needle) {
+        return true;
+    }
+
+    if (cur.value < needle) {
+        return search(cur.right, needle);
+    } else {
+        return search(cur.left, needle);
+    }
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }

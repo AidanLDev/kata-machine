be16a47c41e7e7bbcf6d17ef73c8c8dd9513287c
AidanLDev <dev@aidanlowson.com>
2024-02-18 18:22:27 +0000

    Compare

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: (BinaryNode<number> | null)[] = [head];
 
+    while (queue.length) {
+        const cur = queue.shift() as BinaryNode<number>;
+
+        // search
+        if (cur.value === needle) {
+            return true;
+        }
+
+        if (cur.left) {
+            queue.push(cur.left);
+        }
+
+        if (cur.right) {
+            queue.push(cur.right);
+        }
+    }
+    return false;
 }
